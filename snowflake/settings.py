2914cd0262d9011a17f2feb025335e5cd3c5f9b2
Vruksha Nikam <50704187+vrukshanikam@users.noreply.github.com>
2019-12-11 17:28:49 +0530

    Update settings.py

--- snowflake/settings.py
+++ snowflake/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i(1#p&f4_a(p$*#ou3q1w89capl+fm4d22=rp&)#c#_%etxw*a'
+SECRET_KEY = '****'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
